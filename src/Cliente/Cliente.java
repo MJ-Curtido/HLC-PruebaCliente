c83097ae51d7ab319b382b267f9550026aecd698
MJ-Curtido <manu17pc@gmail.com>
2022-11-07 14:40:16 +0100

    first commit

---
+++ src/Cliente/Cliente.java
@@ -0,0 +1,24 @@
+package Cliente;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class Cliente {
+
+	public static void main(String[] args) throws IOException {
+		String host = "localhost";
+		int puertoRemoto = 6007;
+
+		Socket cliente = new Socket(host, puertoRemoto);
+		
+		DataOutputStream msgCliente = new DataOutputStream(cliente.getOutputStream());
+		msgCliente.writeUTF("Saludos al servidor desde el cliente");
+		
+		DataInputStream msgServidor = new DataInputStream(cliente.getInputStream());
+		System.out.println(msgServidor.readUTF());
+		
+		cliente.close();
+	}
+}
